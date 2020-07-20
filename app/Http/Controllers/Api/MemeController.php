3a5f9dee06f866fa9994ec798c0a43b1378264cf
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-07-19 21:34:41 -0300

    adicionando funções CRUD no controller de memes

--- app/Http/Controllers/Api/MemeController.php
+++ app/Http/Controllers/Api/MemeController.php
@@ -21,5 +21,31 @@ class MemeController extends Controller
         $meme->year = $request->year;
         $meme->upload_date = now();
         $meme->save();
+
+        return response()->json('Meme adicionado com sucesso!');
+    }
+
+    public function showMemeById(Request $request, $id){
+        $meme = Meme::find($id);
+
+        return response()->json($meme);
+    }
+
+    public function editMemeById(Request $request, $id){
+        $meme = Meme::find($id);
+        $meme->name = $request->name;
+        $meme->link = $request->link;
+        $meme->year = $request->year;
+        $meme->upload_date = now();
+        $meme->save();
+
+        return response()->json('Meme editado com sucesso!');
+    }
+
+    public function deleteMemeById(Request $request, $id){
+        $meme = Meme::find($id);
+        $meme->delete();
+
+        return response()->json('Meme deletado com sucesso!');
     }
 }
