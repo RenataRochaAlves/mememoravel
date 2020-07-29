6d2aceb2936b14c620c57783bfe040253e320ba3
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-07-28 23:17:56 -0300

    adicionando template

---
+++ resources/views/template.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baú de Memes</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h1>baú de memes</h1>
+        </div> 
+        <div class="busca">
+            <input type="text" name="busca" id="busca" value="">
+        </div>
+    </header>
+</body>
+</html>
